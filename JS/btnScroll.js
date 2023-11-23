122b177133f525dd5dd44a3e912dc5ace83551ac
Lucas Adriano <134859091+L7643@users.noreply.github.com>
2023-11-23 02:24:56 +0000

    fix js scroll

--- JS/btnScroll.js
+++ JS/btnScroll.js
@@ -9,22 +9,22 @@ const hatchs = document.getElementById('hatch')
 const sports = document.getElementById('esportivos')
 
 function scrollSedan() {
-    sedans.scrollTo(0, 10);
+    sedans.scrollTo(10, 0);
 }
 btnSedan.addEventListener('click', scrollSedan)
 
 function scrollSuv() {
-    suvs.scrollTo(0, 10);
+    suvs.scrollTo(10, 0);
 }
 btnSuv.addEventListener('click', scrollSuv)
 
 function scrollHatch() {
-    hatchs.scrollTo(0, 10);
+    hatchs.scrollTo(10, 0);
 }
 btnHatch.addEventListener('click', scrollHatch)
 
 function scrollSports() {
-    sports.scrollTo(0, 10);
+    sports.scrollTo(10, 0);
 }
 btnSport.addEventListener('click', scrollSports)
 

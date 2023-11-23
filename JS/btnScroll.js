22078f4608085e1c26d0bf43bb0d7ca004158273
Lucas Adriano <134859091+L7643@users.noreply.github.com>
2023-11-23 01:54:01 +0000

    add js scroll

--- JS/btnScroll.js
+++ JS/btnScroll.js
@@ -1,3 +1,31 @@
-let btn = document.getElementsByClassName('scrollDesk')
+const btnSedan = document.getElementById('btnScrollSedan')
+const btnSuv = document.getElementById('btnScrollSuv')
+const btnHatch = document.getElementById('btnScrollHatch')
+const btnSport = document.getElementById('btnScrollSport')
 
-btn.scrollTo(0, 10);
+const sedans = document.getElementById('sedan')
+const suvs = document.getElementById('suv')
+const hatchs = document.getElementById('hatch')
+const sports = document.getElementById('esportivos')
+
+function scrollSedan() {
+    sedans.scrollTo(0, 10);
+}
+btnSedan.addEventListener('click', scrollSedan)
+
+function scrollSuv() {
+    suvs.scrollTo(0, 10);
+}
+btnSuv.addEventListener('click', scrollSuv)
+
+function scrollHatch() {
+    hatchs.scrollTo(0, 10);
+}
+btnHatch.addEventListener('click', scrollHatch)
+
+function scrollSports() {
+    sports.scrollTo(0, 10);
+}
+btnSport.addEventListener('click', scrollSports)
+
+

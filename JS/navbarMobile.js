a7823059bb687cb07e88fd6f10b3e423b215f056
Lucas Adriano <134859091+L7643@users.noreply.github.com>
2023-11-28 01:52:41 +0000

    upgrade in css and mobile responsive

--- JS/navbarMobile.js
+++ JS/navbarMobile.js
@@ -2,25 +2,28 @@
 
 const btnNav = document.querySelectorAll(".btn-nav")
 const out = document.querySelector("main")
-const navbar = document.getElementById('navBarMobile')
 const btnMob = document.getElementById('menuMob')
 
-function ToggleMenu() {
-    navbar.classList.toggle('active')
-
-    if(navbar.classList.contains("active")){
-        btnMob.src = "imagens/fecharHam.png";
-    }else{
-        btnMob.src = "imagens/menuHam.png";
+function ToggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault(); {
+        const navbar = document.getElementById('navBarMobile')
+        navbar.classList.toggle('active')
+        const active = navbar.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        } 
     }
 }
 
+btnMob.addEventListener('click', ToggleMenu);
+btnMob.addEventListener('touchstart', ToggleMenu);
+
 function RemoveMenu() {
     navbar.classList.remove('active')
-    btnMob.src = "imagens/menuHam.png";
 }
-
-btnMob.addEventListener('click', ToggleMenu)
 
 for(let i = 0; i < btnNav.length; i++){
     btnNav[i].addEventListener('click', RemoveMenu)

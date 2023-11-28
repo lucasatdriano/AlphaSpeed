023f9c6c9c84a86c28d6fcf0e80a54f4f6ee8b77
Lucas Adriano <134859091+L7643@users.noreply.github.com>
2023-11-28 03:31:45 +0000

    ajust menuMob

--- JS/navbarMobile.js
+++ JS/navbarMobile.js
@@ -3,26 +3,23 @@
 const btnNav = document.querySelectorAll(".btn-nav")
 const out = document.querySelector("main")
 const btnMob = document.getElementById('menuMob')
+const nav = document.getElementById('nav')
 
 function ToggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault(); {
-        const navbar = document.getElementById('navBarMobile')
-        navbar.classList.toggle('active')
-        const active = navbar.classList.contains('active');
-        event.currentTarget.setAttribute('aria-expanded', active);
-        if (active) {
-            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-        } else {
-            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-        } 
-    }
+    nav.classList.toggle('active')
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    } 
 }
 
 btnMob.addEventListener('click', ToggleMenu);
-btnMob.addEventListener('touchstart', ToggleMenu);
 
 function RemoveMenu() {
-    navbar.classList.remove('active')
+    nav.classList.remove('active')
 }
 
 for(let i = 0; i < btnNav.length; i++){

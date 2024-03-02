6c454d4cd27eab25ad83c197e5fdcb36bc3ba7e3
Lucas Adriano <134859091+L7643@users.noreply.github.com>
2024-03-02 21:30:36 +0000

    add scroll behavior in scroll top

--- assets/js/scrollTop.js
+++ assets/js/scrollTop.js
@@ -2,11 +2,6 @@
         
 const btnTopo = document.querySelector(".topo")
 
-function BackTop() { 
-    scrollTo(0, 0)
-}
-btnTopo.addEventListener("click", BackTop)
-
 const backToTop = () => {
     if (window.scrollY >= 100) {
         btnTopo.classList.add('showBtn')
@@ -14,6 +9,7 @@ const backToTop = () => {
         btnTopo.classList.remove('showBtn')
     }
 }
+
 window.addEventListener('scroll', function () {
     backToTop()
 })
